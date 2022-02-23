f748b4831c0823988c244ee4a17bdaf986ec3da4
Joseph McLachlan <joejmclachlan@gmail.com>
2022-02-23 18:17:57 -0500

    First functioning page

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ let style = document.createElement('style');
 
 let head = document.head;
 
+let btn = document.querySelectorAll('button');
+
+let colorSelection = "rgba(57,50,154,1)"
+
+let slider = document.getElementById("slider")
+
+let gridSize = document.getElementById("grid-size")
+
+let cyberpunkColor = ["#ff124f", "#ff00a0", "fe75fe", "#7a04eb", "#120458"]
+
+for (const i of btn) {
+i.addEventListener('click', changeColor)
+}
+
+slider.addEventListener('input', sliderEvent)
+
+function sliderEvent() {
+    updateCounter(this.value);
+    clearGrid();
+    createGrid(this.value);
+}
+
+function changeColor() {
+    colorSelection = this.value;
+}
+
 head.appendChild(style);
 
 function styleGrid(size) {
@@ -16,6 +42,10 @@ function createCell() {
     return div;
 }
 
+function clearGrid() {
+    gridInner.innerHTML = "";
+}
+
 function createGrid(size) {
     let gridSize = size * size;
     let cellFrag = document.createDocumentFragment();
@@ -23,10 +53,49 @@ function createGrid(size) {
     for (let i = 0; i < gridSize; i++) {
         cellFrag.appendChild(createCell());
     }
-    gridInner.appendChild(cellFrag);
+    gridInner.appendChild(cellFrag)
+    let cellColor = document.getElementsByClassName("cell");
+    addPen(cellColor);
+    updateCounter(size);
 } 
 
-/* 
-make function that creates grid with grid size input
-create divs = to grid size squared
-*/
+function updateCounter(size) {
+    let sizeValue = size.toString();
+    gridSize.innerHTML = `${sizeValue}x${sizeValue}`;
+}
+
+function addPen(value) {
+    for( const i of value) {
+       i.addEventListener('mouseover', paintColor)
+       
+    }
+}
+
+function paintColor(){
+    if (colorSelection === "cyberpunk") {
+        this.classList.remove('grey')
+        this.style.backgroundColor = cyberpunkColorSelector();
+    } else if (colorSelection === "greyscale") {
+        rgbDarken(this);
+    } else{
+        this.classList.remove('grey')
+        this.style.backgroundColor = `${colorSelection}`;
+    }
+}
+
+function rgbDarken(value) {
+    let cellData = value;
+    if (cellData.classList.contains('grey')) {
+        let rgbValue = cellData.style.backgroundColor.toString().slice(-3,-1);
+        rgbValue = Number(rgbValue);
+        rgbValue += .1;
+        cellData.style.backgroundColor = `rgb(0,0,0,${rgbValue}`;
+    }else {
+        cellData.classList.add("grey");
+        cellData.style.backgroundColor = "rgb(0,0,0,.1)"
+    }
+}
+
+function cyberpunkColorSelector() {
+    return cyberpunkColor[Math.floor(Math.random() * cyberpunkColor.length)]
+}
